70a81a014d6ff88b80f970d7d5ee0f4e475aee4a
furucrmBAO <bao.ha@furucrm.com>
2023-03-15 12:05:37 +0900

    init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const bodyParser = require("body-parser");
+const employeesModel = require("./employees");
+const cors = require("cors");
+
+const employees = employeesModel.EmployeeInfo;
+app.use(cors());
+// body parser configuration
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// cloudinary configuration
+
+app.get("/employees", (request, response) => {
+  response.json(employeesModel.EmployeeInfo);
+});
+
+module.exports = app;
---
+++ employees.js
@@ -0,0 +1,65 @@
+exports.EmployeeInfo = [
+  {
+    id: 1,
+    customerNo: 1,
+    name: "Hoang Kagawa",
+    age: 40,
+    position: "Chief Executive Officer",
+    imageURL:
+      "https://www.furucrm.com/resource/1676963015000/LEADER_SHIP_2/Kagawa.jpg"
+  },
+  {
+    id: 2,
+    customerNo: 2,
+    name: "Yen Ngo",
+    age: 24,
+    position: "HR Manager",
+    imageURL:
+      "https://www.furucrm.com/resource/1676963016000/LEADER_SHIP_5/Yen.jpg"
+  },
+  {
+    id: 3,
+    customerNo: 3,
+    name: "Ha Bao",
+    age: 24,
+    position: "Salesforce Specialist",
+    imageURL:
+      "https://www.furucrm.com/resource/1676963014000/LEADER_SHIP_1/Bao.jpg"
+  },
+  {
+    id: 4,
+    customerNo: 4,
+    name: "Dung Huynh",
+    age: 24,
+    position: "Salesforce Specialist",
+    imageURL:
+      "https://www.furucrm.com/resource/1676963014000/LEADER_SHIP_1/Dung.jpg"
+  },
+  {
+    id: 5,
+    customerNo: 5,
+    name: "Nam Nguyen",
+    age: 24,
+    position: "Salesforce Specialist",
+    imageURL:
+      "https://www.furucrm.com/resource/1676963016000/LEADER_SHIP_4/Nam.jpg"
+  },
+  {
+    id: 6,
+    customerNo: 6,
+    name: "Dat Nguyen",
+    age: 24,
+    position: "Salesforce Specialist",
+    imageURL:
+      "https://www.furucrm.com/resource/1676963014000/LEADER_SHIP_1/Dat.jpg"
+  },
+  {
+    id: 7,
+    customerNo: 7,
+    name: "Luc Nguyen",
+    age: 24,
+    position: "Salesforce Specialist",
+    imageURL:
+      "https://www.furucrm.com/resource/1676963016000/LEADER_SHIP_3/Luc.jpg"
+  }
+];
